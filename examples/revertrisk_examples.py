81479ef55a9be92329be35af999f3197fd6c06e2
WMF2906 <wmf2906@WMF2906s-MBP.fios-router.home>
2024-03-25 15:55:59 -0400

    added examples folder, changed examples.py to revertrisk_examples.py, and added type hints to function

---
+++ examples/revertrisk_examples.py
@@ -0,0 +1,27 @@
+import json
+import requests
+
+def revert_risk_api_request(language: str, revision_id: int):
+    use_auth = False
+    inference_url = 'https://api.wikimedia.org/service/lw/inference/v1/models/' + language + '-reverted:predict'
+
+    if use_auth:
+
+        headers = {
+            'Authorization': 'Bearer YOUR_ACCESS_TOKEN',
+            'User-Agent': 'YOUR_APP_NAME (YOUR_EMAIL_OR_CONTACT_PAGE)',
+            'Content-type': 'application/json'
+        }
+    else:
+        headers = {}
+
+    data = {"rev_id": revision_id}
+    response = requests.post(inference_url, headers=headers, data=json.dumps(data))
+    return response.json()
+
+
+language = "viwiki"
+revision_id = 12345
+response = revert_risk_api_request(language, revision_id)
+
+print(response)
