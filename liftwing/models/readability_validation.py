745994593f636f51b2cd7cb22a376352dfd2516f
WMF2906 <wmf2906@WMF2906s-MBP.fios-router.home>
2024-05-30 14:55:38 -0400

    added readability.py

---
+++ liftwing/models/readability_validation.py
@@ -0,0 +1,29 @@
+from pydantic import BaseModel, Field, model_validator
+from .liftwing_model import LiftwingModel
+from ..utils.validator import validate_payload, check_required_fields
+from typing import Any, Dict
+
+
+class ReadabilityPayload(BaseModel):
+    lang: str = Field(...,
+                      title="Language code of the wiki",
+                      description='A string representing the language code related to the target '
+                                  'wiki. Example: "en" for English Wikipedia.')
+    rev_id: int = Field(...,
+                        title="Revision ID of the edit",
+                        description="The revision id for the wiki identified by the lang parameter.")
+
+    @model_validator(mode='before')
+    def check_fields(cls, values):
+        check_required_fields(values, ['lang', 'rev_id'])
+        return values
+
+
+class ReadabilityAPIModel(LiftwingModel):
+    def __init__(self, base_url="https://api.wikimedia.org/service/lw/inference/v1/models/readability:predict"):
+        super().__init__(base_url)
+
+    @validate_payload(ReadabilityPayload)
+    def request(self, payload:  Dict[str, Any], method: str = "POST",
+                headers: Dict[str, str] = None) -> Dict[str, Any]:
+        return super().request(payload=payload, method=method, headers=headers)
