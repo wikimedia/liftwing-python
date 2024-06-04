79e525a8e23aa49a307c84ebbd1167fc3d0575e6
WMF2906 <wmf2906@wmf2906s-mbp.mynetworksettings.com>
2024-06-04 10:20:34 -0400

    added language

--- liftwing/models/language.py
+++ liftwing/models/language.py
@@ -1,27 +1,26 @@
 import requests
 from liftwing_model import LiftwingModel
 from typing import Any, Dict
-
+import json
 
 class ReadabilityAPIModel(LiftwingModel):
     def __init__(self, base_url="https://api.wikimedia.org/service/lw/inference/v1/models/langid:predict"):
         super().__init__(base_url)
 
-    def request(self, payload: Dict[str, Any], method: str = "POST", headers: Dict[str, str] = None) -> Dict[str, Any]:
+    def request(self, method: str = "POST", headers: Dict[str, str] = None) -> Dict[str, Any]:
         """
         This function makes a POST request to https://api.wikimedia.org/service/lw/inference/v1/models/langid:predict
         using the language parameter and returns a JSON
         language is for the different wiki languages, rev_id is the specific revisions
         """
-        language = payload.get("lang")
-        if language is None:
-            raise ValueError("'lang' parameter is required in the payload.")
-        rev_id = payload.get("rev_id")
-        if rev_id is None:
-            raise ValueError("rev_id is none, add revision id to continue")
+        url = "https://api.wikimedia.org/service/lw/inference/v1/models/langid:predict"
 
+        data = {"text": "Some sample text in any language that we want to identify"}
+
+        
         if headers is None:
             headers = {}
 
-        response = requests.post(self.base_url, json=payload, headers=headers)
+        response = requests.post(url, headers=headers, data=json.dumps(data))
+
         return response.json()
