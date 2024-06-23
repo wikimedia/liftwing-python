93ea14e5d1d65ad508cd54308f9d92896c7d3493
WMF2906 <wmf2906@wmf2906s-mbp.mynetworksettings.com>
2024-06-23 14:32:07 -0400

    added readability

---
+++ liftwing/models/readability.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel, Field
+
+from .liftwing_model import LiftwingModel
+
+
+class ReadabilityPayload(BaseModel):
+    lang: str = Field(
+        ...,
+        title="Language code of the wiki",
+        description="A string representing the language code related to the target "
+        'wiki. Example: "en" for English Wikipedia.',
+    )
+    rev_id: int = Field(
+        ...,
+        title="Revision ID of the edit",
+        description="The revision id for the wiki identified by the lang parameter.",
+        gt=0,
+    )
+
+
+class ReadabilityAPIModel(LiftwingModel):
+    def __init__(
+        self,
+        base_url="https://api.wikimedia.org/service/lw/inference/v1/models/readability:predict",
+    ):
+        super().__init__(base_url, ReadabilityPayload)
